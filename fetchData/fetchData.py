dd76a2eaaa598258a4d9f9369ffaf10230acc8dd
ganti <28899559+ganti@users.noreply.github.com>
2022-10-21 20:43:23 +0200

    Add fetchData with gitpush

--- fetchData/fetchData.py
+++ fetchData/fetchData.py
@@ -1,3 +1,4 @@
+#!/usr/bin/python3
 import sys
 import os
 import os.path
@@ -5,6 +6,9 @@ import shutil
 import pandas as pd
 import numpy as np
 import requests
+import filecmp
+import subprocess
+from datetime import datetime
 
 def download(url, file_name):
     with open(file_name, "wb") as file:
@@ -33,9 +37,6 @@ def getDataFromInternet():
         else:
             sys.exit('File does not exist: '+file_name)
 
-
-    
-
 def getGemeindeverzeichnis():
     file_name = r'./PLZO_CSV_LV95/PLZO_CSV_LV95/PLZO_CSV_LV95.csv'
     if os.path.exists(file_name):
@@ -45,7 +46,7 @@ def getGemeindeverzeichnis():
 
     dfTown = dfTown.reset_index(drop=True)
     dfTown = dfTown.drop(['Zusatzziffer', 'Ortschaftsname', 'Kantonskürzel', 'E', 'N'], axis=1)
-    dfTown = dfTown.rename(columns={'PLZ': 'zipTown', 'BFS-Nr': 'bfs', 'Gemeindename': 'village', 'Kantonskürzel': 'canton', 'Sprache': 'locale'})
+    dfTown = dfTown.rename(columns={'PLZ': 'zipTown', 'BFS-Nr': 'bfs', 'Gemeindename': 'town', 'Kantonskürzel': 'canton', 'Sprache': 'locale'})
     dfTown['zipTown'] = dfTown['zipTown'].astype(int)
     dfTown['bfs'] = dfTown['bfs'].astype(int)
     dfTown = dfTown.drop(['zipTown'], axis=1)
@@ -64,7 +65,7 @@ def getGebaeudeverzeichnis():
     dfStreet = dfStreet.drop(['streetVillage'], axis=1)
     return dfStreet
 
-def sanatizeVillagesWithNumberAtEnd(df):
+def sanatizeTownWithNumberAtEnd(df):
     #sanatize Villages with number at end
     #df = df[df['village'].str.contains('\d', regex= True)]
     villageCleanup = {
@@ -74,37 +75,74 @@ def sanatizeVillagesWithNumberAtEnd(df):
         "Laax GR 2": "Laax GR"
     }
     for old, new in villageCleanup.items():
-        df['village'] = df['village'].replace([old], new)
+        df['town'] = df['town'].replace([old], new)
     return df
 
 def calculateZipShare(df):
     #calculate percentage of zip-share
     df['count_zip'] = df.groupby(['zip'])['zip'].transform('count')
-    df['count_bfs'] = df.groupby(['zip','village'])['village'].transform('count')
+    df['count_bfs'] = df.groupby(['zip','town'])['town'].transform('count')
     df['zip-share'] = df['count_bfs'] / df['count_zip'] * 100
     df['zip-share'] = df['zip-share'].round(decimals=2)
     df['zip-share'].values[df['zip-share'].values > 100] = 100
+    df = df.drop(['count_bfs', 'count_zip' ], axis=1)
     return df
 
-def main():
+def main():    
     getDataFromInternet()
     dfTown = getGemeindeverzeichnis()
     dfStreet = getGebaeudeverzeichnis()
     df = pd.merge(dfStreet, dfTown, on='bfs', how='left')
 
-    df = sanatizeVillagesWithNumberAtEnd(df)
+    df = sanatizeTownWithNumberAtEnd(df)
     df = calculateZipShare(df)
     
-    #cleanup
-    #df = df.drop(['count_bfs', 'count_zip', 'zipTown', 'streetVillage'], axis=1)
     df = df.dropna().drop_duplicates()
-    
+    df = df.loc[:, ["zip","bfs",'town','zip-share', 'locale']]
     #save
     df = df.sort_values(by=[ 'zip-share', 'zip'], ascending=[False, True])
     df.to_json(r'zip.json', orient='records');
+    uploadNewFile()
+    cleanup()
+
+
     
-    print( df[df['zip']==3053] )
-    print(df)
+
+def cleanup():
+    files = [   r'./PLZO_CSV_LV95',
+                r'./ch.swisstopo.amtliches-gebaeudeadressverzeichnis',
+                r'zip.json'
+    ]
+    for f in files:
+        if os.path.exists(f):
+            if os.path.isdir(f): 
+                shutil.rmtree(f, ignore_errors=True)
+            else:
+                os.remove(f)
+
+
+def uploadNewFile():
+    file_new = r'./zip.json'
+    file_org = r'./../swissZIP/v4/data/zip.json'
+    if filecmp.cmp(file_new, file_org):
+        msg = datetime.today().strftime('%Y-%m-%d')+' Update data (no update)'
+    else:
+        msg = datetime.today().strftime('%Y-%m-%d')+' Update data'
+        shutil.copyfile(file_new, file_org)
+    addToGitRepo(file_name=file_org, msg=msg)
+    
+def addToGitRepo(file_name, msg):
+    cmds = [
+        'git config --local core.sshCommand "ssh -i $(pwd)/../id_rsa -F /dev/null" ',
+        "git remote set-url origin git@github.com:ganti/swissZIP.git ",
+        "git add "+ file_name,
+        "git commit -m '"+msg+"' "+file_name,
+        "git push"
+        
+    ]
+    separator = "> /dev/null && "
+    cmd = separator.join(cmds)
+    subprocess.run(cmd, shell=True)
 
 
 if __name__ == "__main__":
