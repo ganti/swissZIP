8ee0254a0f926f929b62cc5d000fe8044d20f936
ganti <28899559+ganti@users.noreply.github.com>
2023-12-20 23:27:39 +0100

    Remove v3

--- swissZIP/v3/inc/cors_header.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-    if(!isset($_GET['debug']) and strcasecmp($_SERVER['REQUEST_METHOD'], 'OPTIONS') === 0) {
-        header('Content-Type: text/plain');
-        header('Access-Control-Allow-Origin: *');
-        header('Access-Control-Allow-Credentials: true');
-        header('Access-Control-Allow-Headers: Origin, Authorization, Content-Type, Accept');
-        exit;
-    }
